e7773b381704e1e1fd6196b8fbf99f9f97282abb
YNotPrimo <ivalinkolev0@gmail.com>
2020-09-05 15:24:56 +0300

    Finished Search Function

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = "") => {
     const movieList = document.getElementById("movie-list");
 
     if (movies.length === 0) {
@@ -14,18 +14,23 @@ const renderMovies = () => {
 
     movieList.innerHTML = "";
 
-    movies.forEach(movie => {
+    const filteredMovies = !filter
+        ? movies :
+        movies.filter(movie => movie.info.title.includes(filter));
+
+
+    filteredMovies.forEach(movie => {
         const movieEl = document.createElement("li");
         const movieInfo = movie.info;
 
         let text = movieInfo.title + " - ";
-        
+
         for (const key in movieInfo) {
             if (key !== "title") {
                 text += `${key}: ${movieInfo[key]}`;
             }
         }
-        
+
         movieEl.textContent = text;
         movieList.append(movieEl);
     });
@@ -56,5 +61,10 @@ const addMovieHandler = () => {
     renderMovies();
 };
 
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById("filter-title").value;
+    renderMovies(filterTerm);
+}
 
 addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
